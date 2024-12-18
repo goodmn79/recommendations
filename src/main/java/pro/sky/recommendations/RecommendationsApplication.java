13d4f5f5c76c65650f0042b517003f571c367d3e
goodmn <goodmn79@gmail.com>
2024-12-18 20:30:19 +0300

    init project

---
+++ src/main/java/pro/sky/recommendations/RecommendationsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.recommendations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationsApplication.class, args);
+    }
+
+}
