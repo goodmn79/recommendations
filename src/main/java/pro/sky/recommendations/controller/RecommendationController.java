55c44c5a427585a054c4d8a1b1e8c78e25580902
goodmn <goodmn79@gmail.com>
2024-12-19 00:13:26 +0300

    добавил слои контроллера, сервиса, репозитория и исключение

---
+++ src/main/java/pro/sky/recommendations/controller/RecommendationController.java
@@ -0,0 +1,23 @@
+package pro.sky.recommendations.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.recommendations.dto.UserRecommendationSet;
+import pro.sky.recommendations.service.RecommendationService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("recommendation")
+@RequiredArgsConstructor
+public class RecommendationController {
+    private final RecommendationService recommendationService;
+
+    @GetMapping("{userId}")
+    public UserRecommendationSet getRecommendations(@PathVariable UUID userId) {
+        return recommendationService.getUserRecommendationSet(userId);
+    }
+}
