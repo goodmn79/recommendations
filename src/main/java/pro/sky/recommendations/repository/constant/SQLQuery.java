585e9f1d1740e43f6cf685275627abe1c8ab1aec
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-12-23 18:37:50 +0400

    Исправление названия таблиц

--- src/main/java/pro/sky/recommendations/repository/constant/SQLQuery.java
+++ src/main/java/pro/sky/recommendations/repository/constant/SQLQuery.java
@@ -6,23 +6,23 @@ public final class SQLQuery {
     public static final String FIND_USER_BY_ID = "SELECT * FROM USERS WHERE ID = ?";
 
     public static final String FIND_USER_BY_RULES_FOR_INVEST500 =
-            "SELECT u.ID AS user_id FROM User u JOIN Transaction t ON u.ID = t.user_id"
-                    + "JOIN Product p ON t.product_id = p.ID GROUP BY u.ID"
+            "SELECT u.ID AS user_id FROM Users u JOIN Transactions t ON u.ID = t.user_id"
+                    + "JOIN Products p ON t.product_id = p.ID GROUP BY u.ID"
                     + "HAVING SUM(CASE WHEN p.type = 'DEBIT' THEN 1 ELSE 0 END) > 0"
                     + "AND SUM(CASE WHEN p.type = 'INVEST' THEN 1 ELSE 0 END) = 0"
                     + "AND SUM(CASE WHEN p.type = 'SAVING' AND t.transaction_type = 'DEPOSIT' THEN t.amount ELSE 0 END) > 1000";
 
     public static final String FIND_USER_BY_RULES_FOR_SIMPLE_CREDIT =
-            "SELECT u.ID AS user_id FROM User u JOIN Transaction t ON u.ID = t.user_id"
-                    + "JOIN Product p ON t.product_id = p.ID GROUP BY u.ID"
+            "SELECT u.ID AS user_id FROM Users u JOIN Transactions t ON u.ID = t.user_id"
+                    + "JOIN Products p ON t.product_id = p.ID GROUP BY u.ID"
                     + "HAVING SUM(CASE WHEN p.type = 'CREDIT' THEN 1 ELSE 0 END) = 0"
                     + "AND (SUM(CASE WHEN p.type = 'DEBIT' AND t.transaction_type = 'DEPOSIT' THEN t.amount ELSE 0 END) > "
                     + "SUM(CASE WHEN p.type = 'DEBIT' AND t.transaction_type = 'WITHDRAW' THEN t.amount ELSE 0 END))"
                     + "AND SUM(CASE WHEN p.type = 'DEBIT' AND t.transaction_type = 'WITHDRAW' THEN t.amount ELSE 0 END) > 100000";
 
     public static final String FIND_USER_BY_RULES_FOR_SIMPLE_TOP_SAVING =
-            "SELECT u.ID AS user_id FROM User u JOIN Transaction t ON u.ID = t.user_id"
-                    + "JOIN Product p ON t.product_id = p.ID GROUP BY u.ID"
+            "SELECT u.ID AS user_id FROM Users u JOIN Transactions t ON u.ID = t.user_id"
+                    + "JOIN Products p ON t.product_id = p.ID GROUP BY u.ID"
                     + "HAVING SUM(CASE WHEN p.type = 'DEBIT' THEN 1 ELSE 0 END) > 0"
                     + "AND (SUM(CASE WHEN p.type = 'DEBIT' AND t.transaction_type = 'DEPOSIT' THEN t.amount ELSE 0 END) >= 50000"
                     + "OR SUM(CASE WHEN p.type = 'SAVING' AND t.transaction_type = 'DEPOSIT' THEN t.amount ELSE 0 END) >= 50000))"
