601a6232e4f14efe38e9e0b039f9ddf6ae7db150
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-12-25 16:37:56 +0400

    добавление новых sql запросов и переписывание старых

--- src/main/java/pro/sky/recommendations/repository/constant/SQLQuery.java
+++ src/main/java/pro/sky/recommendations/repository/constant/SQLQuery.java
@@ -6,12 +6,23 @@ public final class SQLQuery {
     public static final String FIND_USER_BY_ID = "SELECT * FROM USERS WHERE ID = ?";
 
     public static final String FIND_USER_BY_RULES_FOR_INVEST500 =
-            "SELECT u.ID AS user_id FROM Users u JOIN Transactions t ON u.ID = t.user_id JOIN Products p ON t.product_id = p.ID GROUP BY u.ID HAVING SUM(CASE WHEN p.type = 'DEBIT' THEN 1 ELSE 0 END) > 0 AND SUM(CASE WHEN p.type = 'INVEST' THEN 1 ELSE 0 END) = 0 AND SUM(CASE WHEN p.type = 'SAVING' AND t.type = 'DEPOSIT' THEN t.amount ELSE 0 END) > 1000";
+            "SELECT EXISTS (SELECT 1 FROM Transactions t JOIN Users u ON  t.USER_ID = u.ID JOIN Products p ON p.ID = t.PRODUCT_ID WHERE u.ID = ? GROUP BY u.ID HAVING SUM(CASE WHEN p.type = 'DEBIT' THEN 1 ELSE 0 END) > 0 AND SUM(CASE WHEN p.type = 'INVEST' THEN 1 ELSE 0 END) = 0 AND SUM(CASE WHEN p.type = 'SAVING' AND t.type = 'DEPOSIT' THEN t.amount ELSE 0 END) > 1000)";
 
     public static final String FIND_USER_BY_RULES_FOR_SIMPLE_CREDIT =
-            "SELECT u.ID AS user_id FROM Users u JOIN Transactions t ON u.ID = t.user_id JOIN Products p ON t.product_id = p.ID GROUP BY u.ID HAVING SUM(CASE WHEN p.type = 'CREDIT' THEN 1 ELSE 0 END) = 0 AND (SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'DEPOSIT' THEN t.amount ELSE 0 END) > SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'WITHDRAW' THEN t.amount ELSE 0 END)) AND SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'WITHDRAW' THEN t.amount ELSE 0 END) > 100000";
+            "SELECT EXISTS (SELECT 1 FROM Transactions t JOIN Users u ON  t.USER_ID = u.ID JOIN Products p ON p.ID = t.PRODUCT_ID WHERE u.ID = ? GROUP BY u.ID HAVING SUM(CASE WHEN p.type = 'CREDIT' THEN 1 ELSE 0 END) = 0 AND (SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'DEPOSIT' THEN t.amount ELSE 0 END) > SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'WITHDRAW' THEN t.amount ELSE 0 END)) AND SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'WITHDRAW' THEN t.amount ELSE 0 END) > 100000)";
 
     public static final String FIND_USER_BY_RULES_FOR_SIMPLE_TOP_SAVING =
-            "SELECT u.ID AS user_id FROM Users u JOIN Transactions t ON u.ID = t.user_id JOIN Products p ON t.product_id = p.ID GROUP BY u.ID HAVING SUM(CASE WHEN p.type = 'DEBIT' THEN 1 ELSE 0 END) > 0 AND SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'DEPOSIT' THEN t.amount ELSE 0 END) >= 50000 OR SUM(CASE WHEN p.type = 'SAVING' AND t.type = 'DEPOSIT' THEN t.amount ELSE 0 END) >= 50000 AND SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'DEPOSIT' THEN t.amount ELSE 0 END) > SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'WITHDRAW' THEN t.amount ELSE 0 END)";
+            "SELECT EXISTS (SELECT 1 FROM Transactions t JOIN Users u ON  t.USER_ID = u.ID JOIN Products p ON p.ID = t.PRODUCT_ID WHERE u.ID = ? GROUP BY u.ID HAVING SUM(CASE WHEN p.type = 'DEBIT' THEN 1 ELSE 0 END) > 0 AND (SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'DEPOSIT' THEN t.amount ELSE 0 END) >= 50000 OR SUM(CASE WHEN p.type = 'SAVING' AND t.type = 'DEPOSIT' THEN t.amount ELSE 0 END) >= 50000) AND SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'DEPOSIT' THEN t.amount ELSE 0 END) > SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'WITHDRAW' THEN t.amount ELSE 0 END))";
+
+
+
+    public  static final  String USER_OF = "SELECT COUNT(*) > 0 FROM TRANSACTIONS t JOIN USERS u ON u.ID = t.USER_ID JOIN PRODUCTS p ON p.ID = t.PRODUCT_ID WHERE USER_ID = ? AND p.TYPE = ?";
+    public  static  final String ACTIVE_USER_OF = "SELECT COUNT(*) >= 5 FROM TRANSACTIONS t JOIN USERS u ON u.ID = t.USER_ID JOIN PRODUCTS p ON p.ID = t.PRODUCT_ID WHERE USER_ID = ? AND p.TYPE = ?";
+
+    // через String.format() подставляем comparisonOperator %s этот оператор сравнения передается как строка, constant %d передается как число. Необходима проверка через matches()
+    public  static  final String TRANSACTION_SUM_COMPARE = "SELECT EXISTS (SELECT 1 FROM TRANSACTIONS t JOIN USERS u ON u.ID = t.USER_ID JOIN PRODUCTS p ON p.ID = t.PRODUCT_ID WHERE t.USER_ID = ? AND p.TYPE = ? AND t.TYPE = ? GROUP BY u.ID HAVING SUM(t.AMOUNT) %s %d)";
+    // через String.format() подставляем comparisonOperator %s. Необходима проверка через matches()
+    public  static  final String TRANSACTION_SUM_COMPARE_DEPOSIT_WITHDRAW  = "SELECT EXISTS (SELECT 1 FROM TRANSACTIONS t JOIN USERS u ON u.ID = t.USER_ID JOIN PRODUCTS p on p.ID = t.PRODUCT_ID WHERE USER_ID = ? AND p.TYPE = ? GROUP BY u.ID HAVING SUM(CASE WHEN t.TYPE = 'DEPOSIT' THEN t.AMOUNT ELSE 0 END) %s SUM(CASE WHEN t.TYPE = 'WITHDRAW' THEN t.AMOUNT ELSE 0 END))";
+
 }
 
