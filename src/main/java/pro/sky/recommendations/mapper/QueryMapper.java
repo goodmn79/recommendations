f905df77b1b436133f8eaf2b33ae5ef1a4eae7ec
barkhudaryan-arman <armanik147@gmail.com>
2024-12-26 10:25:40 +0300

    fix: Доработал маппер и добавил ошибку

--- src/main/java/pro/sky/recommendations/mapper/QueryMapper.java
+++ src/main/java/pro/sky/recommendations/mapper/QueryMapper.java
@@ -1,14 +1,22 @@
 package pro.sky.recommendations.mapper;
 
+import org.apache.commons.lang3.StringUtils;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Component;
+import pro.sky.recommendations.constant.ComparisonOperator;
+import pro.sky.recommendations.constant.ProductType;
+import pro.sky.recommendations.constant.TransactionType;
 import pro.sky.recommendations.dto.QueryDTO;
+import pro.sky.recommendations.exception.InvalidQueryDataException;
 import pro.sky.recommendations.model.Query;
 
 import java.lang.reflect.Type;
 import java.util.Collection;
+import java.util.UUID;
+
+import static pro.sky.recommendations.constant.QueryType.*;
 
 @Component
 public class QueryMapper {
@@ -19,13 +27,23 @@ public class QueryMapper {
             @Override
             protected void configure() {
                 skip(destination.getId());
-                map(source.getId(), destination.getId());
             }
         });
     }
 
     public Query mapFromDTO(QueryDTO queryDTO) {
-        return mapper.map(queryDTO, Query.class);
+        validateQuery(queryDTO);
+        return mapper.map(queryDTO, Query.class)
+                .setId(UUID.randomUUID());
+    }
+
+    public Collection<Query> mapFromDTO(Collection<QueryDTO> queryDTOS) {
+        queryDTOS.forEach(this::validateQuery);
+        Type collectionType = new TypeToken<Collection<Query>>() {
+        }.getType();
+        Collection<Query> queries = mapper.map(queryDTOS, collectionType);
+        queries.forEach(query -> query.setId(UUID.randomUUID()));
+        return queries;
     }
 
     public QueryDTO mapToDTO(Query query) {
@@ -38,9 +56,25 @@ public class QueryMapper {
         return mapper.map(queries, collectionType);
     }
 
-    public Collection<Query> mapFromDTO(Collection<QueryDTO> queryDTOS) {
-        Type collectionType = new TypeToken<Collection<Query>>() {
-        }.getType();
-        return mapper.map(queryDTOS, collectionType);
+    private void validateQuery(QueryDTO queryDTO) {
+        if (!QUERY_TYPES.containsKey(queryDTO.getQuery())
+                || !validArgs(queryDTO)
+                || queryDTO.getNegate() == null) {
+            throw new InvalidQueryDataException();
+        }
+    }
+
+    private boolean validArgs(QueryDTO queryDTO) {
+        String queryType = queryDTO.getQuery();
+        String[] args = queryDTO.getArgs();
+        return switch (queryType) {
+            case (USER_OF), (ACTIVE_USER_OF) -> args.length == 1 && ProductType.PRODUCT_TYPES.contains(args[0]);
+            case (TRANSACTION_SUM_COMPARE_DEPOSIT_WITHDRAW) -> args.length == 4
+                    && ProductType.PRODUCT_TYPES.contains(args[0])
+                    && TransactionType.TRANSACTION_TYPES.contains(args[1])
+                    && ComparisonOperator.COMPARISON_OPERATORS.contains(args[2])
+                    && StringUtils.isNumeric(args[3]);
+            default -> false;
+        };
     }
 }
