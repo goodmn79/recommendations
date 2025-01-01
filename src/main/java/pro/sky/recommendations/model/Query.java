bb194653642e5374d4345ee7ec7a27896f06e815
goodmn <goodmn79@gmail.com>
2025-01-01 11:01:20 +0300

    закоментировал файлы моделей

--- src/main/java/pro/sky/recommendations/model/Query.java
+++ src/main/java/pro/sky/recommendations/model/Query.java
@@ -1,10 +1,13 @@
+/*
+Powered by ©AYE.team
+ */
+
 package pro.sky.recommendations.model;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -16,12 +19,14 @@ public class Query {
     private String[] arguments;
     private Boolean negate;
 
+    // Преобразование массива аргументов в строку для сохранения в базу данных
+    public String argsToString() {
+        return StringUtils.join(this.arguments, " ");
+    }
+
+    // Преобразование строки с аргументами в массив аргументов
     public Query stringToArgs(String args) {
         this.arguments = args.split(" ");
         return this;
-    }
-
-    public String argsToString() {
-        return StringUtils.join(this.arguments, " ");
     }
 }
