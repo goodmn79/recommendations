2b1075e8a35ee145123dcaa3d808f01d96db726c
goodmn <goodmn79@gmail.com>
2025-01-01 07:39:16 +0300

    изменил имя метода в UserRecommendationService

---
+++ src/test/java/pro/sky/recommendations/controller/UserRecommendationControllerTest.java
@@ -0,0 +1,12 @@
+package pro.sky.recommendations.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRecommendationControllerTest {
+
+    @Test
+    void userRecommendation() {
+    }
+}
