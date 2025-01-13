1175bfc2009d40e697d096f577faa31461235615
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2025-01-13 16:30:13 +0400

    #73 зарегистрировала ТГ-бота, создала файл конфигурации

---
+++ src/main/java/pro/sky/recommendations/configuration/TelegramBotConfiguration.java
@@ -0,0 +1,23 @@
+/*
+Файл конфигурации для подключения телеграм-бота
+Powered by ©AYE.team
+ */
+
+package pro.sky.recommendations.configuration;
+
+import com.pengrad.telegrambot.TelegramBot;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramBotConfiguration {
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    @Bean
+    public TelegramBot telegramBot() {
+        TelegramBot bot = new TelegramBot(token);
+        return bot;
+    }
+}
