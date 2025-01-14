efdbaefccd3072640c3fb6aec0e380eab0bdfb0d
goodmn79 <goodmn79@gmail.com>
2025-01-14 12:42:09 +0300

    #79 создал слои контроллера и сервиса для представления информации о приложении

--- src/main/java/pro/sky/recommendations/service/ManagementService.java
+++ src/main/java/pro/sky/recommendations/service/ManagementService.java
@@ -1,18 +1,29 @@
 /*
 Файл сервиса для управления данными приложения
 Powered by ©AYE.team
- */
+*/
 
 package pro.sky.recommendations.service;
 
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
+import pro.sky.recommendations.dto.InfoManager;
 
 @Service
+@RequiredArgsConstructor
 public class ManagementService {
+    private final InfoManager infoManager;
+
     private final Logger log = LoggerFactory.getLogger(ManagementService.class);
+
+    // Получениесервмсной информации
+    public InfoManager infoManager() {
+        log.info("Fetching info data");
+        return infoManager;
+    }
 
     // Очистка кэша
     @CacheEvict(value = "userRecommendationCache", allEntries = true)
@@ -20,3 +31,4 @@ public class ManagementService {
         log.info("Clearing cache");
     }
 }
+
