9f6155796c27dff23f5436a679e74425e836e24c
goodmn79 <goodmn79@gmail.com>
2025-01-26 17:08:35 +0300

    #86 сделал рефакторинг unit-тестов StatsService, изменил название метода StatsService

--- src/test/java/pro/sky/recommendations/stats/service/StatsServiceTest.java
+++ src/test/java/pro/sky/recommendations/stats/service/StatsServiceTest.java
@@ -2,108 +2,134 @@ package pro.sky.recommendations.stats.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import pro.sky.recommendations.dto.UserRecommendation;
-import pro.sky.recommendations.model.Recommendation;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pro.sky.recommendations.recommendation.dto.RecommendationData;
+import pro.sky.recommendations.recommendation.model.Recommendation;
 import pro.sky.recommendations.stats.dto.StatsData;
 import pro.sky.recommendations.stats.mapper.StatsMapper;
 import pro.sky.recommendations.stats.model.Stats;
 import pro.sky.recommendations.stats.repository.StatsRepository;
+import pro.sky.recommendations.user_recommendation.dto.UserRecommendation;
 
-
+import java.lang.reflect.Field;
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
-
+@ExtendWith(MockitoExtension.class)
 class StatsServiceTest {
     @Mock
     private StatsMapper statsMapper;
-
     @Mock
     private StatsRepository statsRepository;
-
     @InjectMocks
     private StatsService statsService;
 
     private Recommendation recommendation;
     private Stats stats;
+    StatsData statsData;
+    Map<UUID, Stats> testStatsCounters;
 
     @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        recommendation = new Recommendation();
-        recommendation.setId(UUID.randomUUID());
+    void setUp() throws Exception {
+        recommendation = new Recommendation()
+                .setId(UUID.randomUUID());
 
-        stats = new Stats();
-        stats.setRecommendation(recommendation);
-        stats.setCount(0);
+        stats = new Stats()
+                .setRecommendation(recommendation)
+                .setCount(0);
+
+        statsData = new StatsData()
+                .setRecommendationId(stats.getRecommendation().getId())
+                .setCount(stats.getCount());
+
+        testStatsCounters = new HashMap<>(Map.of(recommendation.getId(), stats));
+        Field field = statsService.getClass().getDeclaredField("statsCounters");
+        field.setAccessible(true);
+        field.set(statsService, testStatsCounters);
     }
 
     @Test
-    void testInitStatsDataStore_WhenNoDataInRepository() {
+    void initStatsDataStore_whenValuesExistInDatabase_shouldInitMap() {
+        when(statsRepository.findAll()).thenReturn(Collections.singletonList(stats));
+        when(statsMapper.toStatsDataList(Collections.singletonList(stats)))
+                .thenReturn(Collections.singletonList(statsData));
+
+        statsService.initStatsDataStore();
+
+        verify(statsRepository, times(1)).findAll();
+
+        assertThat(statsService.getAll()).isNotEmpty();
+        assertThat(statsService.getAll()).contains(statsData);
+    }
+
+    @Test
+    void initStatsDataStore_whenNoValuesInDatabase_shouldInitMapWithValues() {
         when(statsRepository.findAll()).thenReturn(Collections.emptyList());
 
         statsService.initStatsDataStore();
 
-        assertTrue(statsService.getStatsList().isEmpty());
+        verify(statsRepository, times(1)).findAll();
 
+        assertTrue(statsService.getAll().isEmpty());
     }
 
     @Test
-    void testIncrementor(){
-        UserRecommendation userRecommendation = mock(UserRecommendation.class);
-        when(userRecommendation.getRecommendations()).thenReturn(List.of(recommendation));
-
-        statsService.initStatsDataStore();
-        statsService.createCounter(recommendation);
-        statsService.incrementor(userRecommendation);
-
-        assertEquals(1, statsService.getStatsList().stream()
-                .filter(s -> s.getRecommendation().equals(recommendation))
-                .findFirst()
-                .map(Stats::getCount)
-                .orElse(0));
-    }
-
-    @Test
-    void testCreateCounter(){
-        statsService.createCounter(recommendation);
-
-        assertEquals(1, statsService.getStatsList().size());
-    }
-
-    @Test
-    void testDeleteCounter(){
-        statsService.createCounter(recommendation);
-
-        statsService.deleteCounter(recommendation);
-
-        assertTrue(statsService.getStatsList().isEmpty());
-    }
-
-    @Test
-    void testGetAll(){
-        List<Stats> statsList = Collections.singletonList(stats);
-        when(statsMapper.toStatsDataList(statsList)).thenReturn(Collections.singletonList(new StatsData()));
-
-        statsService.initStatsDataStore();
-
-        List<StatsData> statsDataList = statsService.getAll();
-
-        assertNotNull(statsDataList);
-        assertEquals(1, statsDataList.size());
-    }
-
-    @Test
-    void testSave(){
-        statsService.createCounter(recommendation);
+    void testSave() {
+        doNothing().when(statsRepository).saveAll(anyList());
 
         statsService.save();
 
         verify(statsRepository, times(1)).saveAll(anyList());
+    }
+
+    @Test
+    void statsAccumulatorTest() {
+        int count = stats.getCount();
+        RecommendationData recommendationData = new RecommendationData()
+                .setId(recommendation.getId());
+        UserRecommendation userRecommendation = new UserRecommendation()
+                .setRecommendations(Collections.singletonList(recommendationData));
+
+        statsService.statsAccumulator(userRecommendation);
+
+        assertThat(stats.getCount()).isEqualTo(++count);
+    }
+
+    @Test
+    void testGetAll() {
+        List<Stats> expected = Collections.singletonList(stats);
+        when(statsMapper.toStatsDataList(anyList()))
+                .thenReturn(Collections.singletonList(statsData));
+
+        List<StatsData> actual = statsService.getAll();
+
+        assertThat(actual).isNotEmpty();
+        assertThat(actual.size()).isEqualTo(expected.size());
+        assertThat(actual).contains(statsData);
+    }
+
+    @Test
+    void testCreateCounter() {
+        Recommendation newRecommendation = new Recommendation()
+                .setId(UUID.randomUUID());
+
+        statsService.createCounter(newRecommendation);
+
+        assertThat(testStatsCounters.size()).isEqualTo(2);
+    }
+
+    @Test
+    void testDeleteCounter() {
+
+        statsService.deleteCounter(recommendation.getId());
+
+        assertThat(testStatsCounters.get(recommendation.getId())).isNull();
+        assertThat(testStatsCounters).isEmpty();
     }
 }
